6fde34a070302fa9386b6fbd6a8b241dd9eea02e
Bender97 <bopallino@gmail.com>
2020-05-27 12:45:15 +0200

    4: added text

--- engine.py
+++ engine.py
@@ -16,12 +16,12 @@ HEALED = 3
 DEAD = 4
 
 color = [
-	(255, 255, 255), # WHITE
-	(255, 50, 50), # RED
-	(255, 255, 0), # YELLOW
-	(50, 255, 50) # GREEN
-	]
-	# no color for the dead (removed)
+    (255, 255, 255), # WHITE
+    (255, 50, 50), # RED
+    (255, 255, 0), # YELLOW
+    (50, 255, 50) # GREEN
+    ]
+    # no color for the dead (removed)
 
 CIRCLE_RADIUS = 5
 INF_RADIUS = 30
@@ -41,47 +41,53 @@ paused = False
 
 # Ball setup [x, y] = [col, row]
 people = []
+time = 0
 
 def generateWalkers():
-	for i in range(NO_PEOPLE):
-		people.append(Walker(WIDTH, HEIGHT))
+    for i in range(NO_PEOPLE):
+        people.append(Walker(WIDTH, HEIGHT))
 
 def generateInfected():
-	if (NO_INITIAL_INFECTED > NO_PEOPLE):
-		print("troppi infettati, sono pi delle persone disponibili!")
-		exit()
-	infected = random.sample(people, NO_INITIAL_INFECTED)
-	for person in infected:
-		person.status = INFECTED
+    if (NO_INITIAL_INFECTED > NO_PEOPLE):
+        print("troppi infettati, sono pi delle persone disponibili!")
+        exit()
+    infected = random.sample(people, NO_INITIAL_INFECTED)
+    for person in infected:
+        person.status = INFECTED
 
-	people.sort(key = lambda x: x.status, reverse=True)
+    people.sort(key = lambda x: x.status, reverse=True)
 
 def distance(p1, p2):
-	return math.sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
+    return math.sqrt((p2.x - p1.x)**2 + (p2.y - p1.y)**2)
 
 def spreadInfection():
-	for i in range(no_infected, NO_PEOPLE):
-		# se non sei infetto, per ogni infetto controlla se sei nella sua zona di infezione
-		for j in range(no_infected):
-			if (distance(people[i], people[j]) < INF_RADIUS):
-				# se sei dentro, lancia il dado
-				if (random.random()>INF_PROB):
-					people[i].status = 	INFECTED #generare asintomatici!!
-	people.sort(key = lambda x: x.status, reverse=True)
+    for i in range(no_infected, NO_PEOPLE):
+        # se non sei infetto, per ogni infetto controlla se sei nella sua zona di infezione
+        for j in range(no_infected):
+            if (distance(people[i], people[j]) < INF_RADIUS):
+                # se sei dentro, lancia il dado
+                if (random.random()>INF_PROB):
+                    people[i].status =  INFECTED #generare asintomatici!!
+    people.sort(key = lambda x: x.status, reverse=True)
 
 def update():
-	# prima aggiorniamo le posizioni delle persone
-	# poi permettiamo il contagio
-	for person in people:
-		person.update(WIDTH, HEIGHT)
+    # prima aggiorniamo le posizioni delle persone
+    # poi permettiamo il contagio
+    for person in people:
+        person.update(WIDTH, HEIGHT)
 
-	spreadInfection()
+    spreadInfection()
 
 def render():
     screen.fill(BLACK)
     for person in people:
-    	pygame.draw.circle(screen, color[person.status], (person.x, person.y), CIRCLE_RADIUS, 0)
-    	pygame.draw.circle(screen, color[person.status], (person.x, person.y), INF_RADIUS, 1)
+        pygame.draw.circle(screen, color[person.status], (person.x, person.y), CIRCLE_RADIUS, 0)
+        pygame.draw.circle(screen, color[person.status], (person.x, person.y), INF_RADIUS, 1)
+    
+    font = pygame.font.Font(None, 36)
+    text = font.render("time " + str(time), 1, (255, 255, 255))
+    textpos = text.get_rect(centerx = WIDTH/2)
+    screen.blit(text, textpos)
     pygame.display.update()
     fps.tick(30)
 
@@ -101,3 +107,4 @@ while True:
     if not paused:
         update()
         render()
+        time+=1
