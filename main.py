a577750cbdb24f68d0e02d136ec05dec363fe991
Bender97 <bopallino@gmail.com>
2020-05-28 12:37:03 +0200

    prova

---
+++ main.py
@@ -0,0 +1,23 @@
+from structures.locations import *
+import random
+
+from engine.virus import Virus
+from walkers.Walker import Walker
+
+import walkers.healthState as h
+
+virus = Virus(range = 40, pInfection = 1 , healthParams = 1, healingParams = 1)
+
+loc = buildDefaultLocation(HOME)
+loc.initRendering()
+
+for i in range(20):
+    loc.enter(Walker(640, 480, 18, 1))
+
+# HARDCODED
+loc.walkers[0].setStatus(h.INFECTED)
+loc.no_infected = 1
+while(True):
+    loc.update(virus)
+    print(loc.no_infected)
+    loc.render(virus)
