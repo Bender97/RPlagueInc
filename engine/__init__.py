d8f9073b478d7ee318230276caa4117b78f6ce4a
Bender97 <bopallino@gmail.com>
2020-05-28 15:09:32 +0200

    engine Enviroment folders name are now compliant and compilable

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,6 +1,6 @@
 from gym.envs.registration import register
 
 register(
-    id='foo-v0',
-    entry_point='gym_foo.envs:FooEnv',
+    id='engine-v0',
+    entry_point='engine.envs:EngineEnv',
 )
--- engine/envs/__init__.py
+++ engine/envs/__init__.py
@@ -1,1 +1,1 @@
-from gym_foo.envs.foo_env import FooEnv
+from engine.envs.engine import EngineEnv
