7f7c528a6da4aed8e4c7e6a3f8f0dfbcd9f49626
Bender97 <bopallino@gmail.com>
2020-05-30 17:31:35 +0200

    debugging

--- engine/popgen.py
+++ engine/popgen.py
@@ -16,4 +16,13 @@ def genPopulation(region):
             generatedPeople = random.randint(1, maxPeople)
             for i in range(0, generatedPeople):
                 w = Walker(Gdict[key].size_x, Gdict[key].size_y, random.randint(1, 100), random.random(), Gdict[key], key)
+                
+                ##############àààà RIGHE AGGIUNTE PER DIVERTIMENTO - da cancellare
+
+                if (random.random()<0.2):
+                    w.setStatus(h.INCUBATION)
+                    w.updateVirusTimer(value = random.randint(h.INCUBATION_DURATION_RANGE[0], h.INCUBATION_DURATION_RANGE[1]))
+                
+                ##############àààà end
+
                 Gdict[key].enter(w)
