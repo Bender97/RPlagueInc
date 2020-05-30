3310f4309c7f0e23d73b90459c3d988fd31bad0e
Bender97 <bopallino@gmail.com>
2020-05-30 14:41:14 +0200

    finished dynamic plots

--- mainFusy.py
+++ mainFusy.py
@@ -10,36 +10,12 @@ import time
 
 import matplotlib.pyplot as plt
 
-class DynamicUpdate():
-    #Suppose we know the x range
-
-    def on_launch(self):
-        #Set up plot
-        self.figure, self.ax = plt.subplots()
-        self.lines, = self.ax.plot([],[], 'ro-')
-        #Autoscale on unknown axis and known lims on the other
-        self.ax.set_autoscaley_on(True)
-        #Other stuff
-        self.ax.grid()
-        ...
-
-    def on_running(self, xdata, ydata):
-        #Update data (with the new _and_ the old points)
-        self.lines.set_xdata(xdata)
-        self.lines.set_ydata(ydata)
-        #Need both of these in order to rescale
-        self.ax.relim()
-        self.ax.autoscale_view()
-        #We need to draw *and* flush
-        self.figure.canvas.draw()
-        self.figure.canvas.flush_events()
-
 virus = Virus(range = 40, pInfection = 1 , healthParams = 1, healingParams = 1)
 
 loc = buildDefaultLocation(HOME)
 loc.initRendering()
 
-for i in range(20):
+for i in range(100):
                     
     walker = Walker( 640, # loc_width
                      480, # loc_height
@@ -55,12 +31,9 @@ for i in range(20):
     loc.enter(walker)
 
 day_counter = 0
-
-d = DynamicUpdate()
-plt.ion()
-d.on_launch()
 xdata = []
-ydata = []
+ydata = [[], [], [], []]
+
 
 while(True):
     for hour in range(24):
@@ -90,10 +63,20 @@ while(True):
                     loc.walkers[h.RECOVERED].append(infected)
         
         xdata.append(day_counter*24 + hour)
-        ydata.append(len(loc.walkers[h.SUSCEPTIBLE]))
+        #ydata.append(len(loc.walkers[h.SUSCEPTIBLE]))
 
-        d.on_running(xdata, ydata)
+        
+        ydata[0].append(len(loc.walkers[h.SUSCEPTIBLE]))
+        ydata[1].append(len(loc.walkers[h.INFECTED]) + len(loc.walkers[h.ASYMPTOMATIC]) + len(loc.walkers[h.INCUBATION]))
+        ydata[2].append(len(loc.walkers[h.RECOVERED]))
+        ydata[3].append(len(loc.walkers[h.DEAD]))
+        
+        plt.plot(xdata, ydata[0], 'bo-')
+        plt.plot(xdata, ydata[1], 'ro-')
+        plt.plot(xdata, ydata[2], 'go-')
+        plt.plot(xdata, ydata[3], 'ko-')
 
+        plt.pause(0.1)
         loc.render(virus)
         time.sleep(0.5)
 
