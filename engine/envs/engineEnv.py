9d7da8dbf2ebc3003219f611b5b2c4d40768d99e
samuel cognolato <samuel.cognolato@gmail.com>
2020-06-30 09:56:08 +0100

    Revert "Added numbers on graphs"
    This reverts commit 4a505f79bf86b5f9a9b3f2d3e31d73f61a84ee46.

--- engine/envs/engineEnv.py
+++ engine/envs/engineEnv.py
@@ -300,17 +300,7 @@ class EngineEnv(gym.Env):
     def render(self, mode='human'):
         renderFramePyGame(engine = self)
         renderFramePlt(engine = self)
-
-        # scale the observations to compose the graphs
-        graphs = self.observations.copy()
-        graphs[0] *= param.ALPHA
-        graphs[1] *= param.BETA
-        graphs[2] *= param.GAMMA
-        graphs[3] *= param.DELTA
-        graphs[4] *= param.EPSILON
-        graphs.append(sum(graphs))
-
-        renderFramePltState(engine = self, state = graphs)
+        renderFramePltState(engine = self, state = self.observations)
         #time.sleep(0.1)
 
     # def close(self):
